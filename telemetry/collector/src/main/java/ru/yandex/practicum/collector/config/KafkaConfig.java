aa43d558ed44322955d19e6cf366ae980d1410e3
gabudakate <saleevakate@yandex.ru>
2026-07-24 13:48:03 +0500

    Исправлены замечания

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaConfig.java
@@ -1,12 +1,12 @@
 package ru.yandex.practicum.collector.config;
 
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
 import ru.yandex.practicum.collector.serialization.GeneralAvroSerializer;
 
 import java.util.HashMap;
@@ -15,19 +15,15 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-    private static final String BOOTSTRAP_SERVERS = "localhost:9092";
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
 
     @Bean
-    public ProducerFactory<String, Object> producerFactory() {
+    public KafkaProducer<String, SpecificRecordBase> kafkaProducer() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class);
-        return new DefaultKafkaProducerFactory<>(props);
-    }
-
-    @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate() {
-        return new KafkaTemplate<>(producerFactory());
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, GeneralAvroSerializer.class.getName());
+        return new KafkaProducer<>(props);
     }
 }
--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/EventServiceImpl.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/service/EventServiceImpl.java
@@ -1,8 +1,11 @@
 package ru.yandex.practicum.collector.service;
 
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.collector.mapper.HubEventMapper;
 import ru.yandex.practicum.collector.mapper.SensorEventMapper;
@@ -19,21 +22,43 @@ public class EventServiceImpl implements EventService {
     private static final String SENSOR_TOPIC = "telemetry.sensors.v1";
     private static final String HUB_TOPIC = "telemetry.hubs.v1";
 
-    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final KafkaProducer<String, SpecificRecordBase> kafkaProducer;
     private final SensorEventMapper sensorEventMapper;
     private final HubEventMapper hubEventMapper;
 
     @Override
     public void sendSensorEvent(SensorEvent event) {
         SensorEventAvro avroEvent = sensorEventMapper.toAvro(event);
-        kafkaTemplate.send(SENSOR_TOPIC, event.getHubId(), avroEvent);
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(
+                SENSOR_TOPIC,
+                null,
+                event.getTimestamp().toEpochMilli(),
+                event.getHubId(),
+                avroEvent
+        );
+        kafkaProducer.send(record);
         log.info("Отправлено событие датчика в топик  {}: {}", SENSOR_TOPIC, avroEvent);
     }
 
     @Override
     public void sendHubEvent(HubEvent event) {
         HubEventAvro avroEvent = hubEventMapper.toAvro(event);
-        kafkaTemplate.send(HUB_TOPIC, event.getHubId(), avroEvent);
+        ProducerRecord<String, SpecificRecordBase> record = new ProducerRecord<>(
+                HUB_TOPIC,
+                null,
+                event.getTimestamp().toEpochMilli(),
+                event.getHubId(),
+                avroEvent
+        );
+        kafkaProducer.send(record);
         log.info("Отправлено событие хаба в топик {}: {}", HUB_TOPIC, avroEvent);
+    }
+
+    @PreDestroy
+    public void close() {
+        if (kafkaProducer != null) {
+            kafkaProducer.flush();
+            kafkaProducer.close();
+        }
     }
 }
